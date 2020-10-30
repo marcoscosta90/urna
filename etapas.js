bdfd4daf3850623ce59632f3562c81b094855017
marcos <marcoscosta.dev@gmail.com>
2020-10-30 11:06:30 -0300

    alteracao final

---
+++ etapas.js
@@ -0,0 +1,55 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '12345',
+                nome: 'Fallen',
+                partido: 'CS',
+                fotos: [
+                    {url:'fallen.jpg', legenda: 'Vereador'}
+                ]          
+            },
+            {
+                numero: '12398',
+                nome: 'Coldzera',
+                partido: 'CS',
+                fotos: [
+                    {url:'coldezera.jpg', legenda: 'Vereador'  }
+                ]          
+            }
+        
+        ]
+    },
+
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '12',
+                nome: 'Fallen',
+                partido: 'CS',
+                vice: 'Coldezera',
+                fotos: [
+                    {url:'fallen.jpg', legenda: 'Prefeito'},
+                    {url:'coldezera.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]          
+            },
+            {
+                numero: '34',
+                nome: 'Coldzera',
+                partido: 'CS',
+                vice:'Fallen',
+                fotos: [
+                    {url:'coldezera.jpg', legenda: 'Prefeito'  },
+                    {url:'fallen.jpg', legenda: 'Vice-Prefeito' , small: true }
+                ]          
+            }
+        
+        ]
+    }
+
+]
